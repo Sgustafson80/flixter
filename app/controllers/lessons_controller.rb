a62920ceb41d75ae250a66b71f8f136fe084a532
Sarah Gustafson <sgustafson80@gmail.com>
2019-10-06 18:31:23 +0000

    made user restrictions work

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -8,7 +8,7 @@ class LessonsController < ApplicationController
   private
 
   def require_authorized_for_current_lesson
-    if current_lesson.section.course != current_user.enrolled_in?
+    if current_user.enrolled_in?(current_lesson.section.course)
       redirect_to course_path, alert: 'You are not enrolled!'
     end
   end
